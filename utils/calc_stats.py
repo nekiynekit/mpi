db1feca437a0d45b14eac66151c2c2856d90795c
Nikita Romanov <Nikita.Romanov@e-kama.com>
2024-04-18 00:16:56 +0300

    Global restyle

--- utils/calc_stats.py
+++ utils/calc_stats.py
@@ -9,15 +9,13 @@ for length in lens:
 
     length = int(length)
 
-    preprompt = f"python create_data_for_task_2.py --length {length} --minv 10 --maxv 1000"
+    preprompt = f"python utils/create_data_for_task_2.py --length {length} --minv 10 --maxv 1000"
     run(preprompt)
     for proc_num in range(3, 13):
 
         filename = f"data/stat_{proc_num}_{length}"
 
-        prompt = (
-            f"mpiexec -n {proc_num} python task_2.py --filename data/array.in --length {length} --no-output > {filename}"
-        )
+        prompt = f"mpiexec -n {proc_num} python sources/task_2.py --filename data/array.in --length {length} --no-output > {filename}"
         run(prompt)
 
         with open(filename, "r") as file:
@@ -26,8 +24,13 @@ for length in lens:
         logs[length][0].append(proc_num)
         logs[length][1].append(lag)
 
+plt.xlabel("proc nums")
+plt.ylabel("exec time")
+plt.grid()
 for length in lens:
-    # if length == int(1e7):
-    plt.plot(*logs[length])
-    # break
+    x, y = logs[length]
+    plt.plot(x, y, label=str(length))
+plt.legend(loc="upper right")
+
+plt.savefig("plots/result_1.png")
 plt.show()
--- utils/calc_stats_2.py
+++ utils/calc_stats_2.py
@@ -9,13 +9,11 @@ for length in lens:
 
     length = int(length)
     for proc_num in range(2, 13):
-        print(f"Processing len={length}, proc_num={proc_num}...", end='\r')
+        print(f"Processing len={length}, proc_num={proc_num}...", end="\r")
 
         filename = f"data/stat_task3_{proc_num}_{length}"
 
-        prompt = (
-            f"mpiexec -n {proc_num} python task_3.py -x {length} --no-output > {filename}"
-        )
+        prompt = f"mpiexec -n {proc_num} python sources/task_3.py -x {length} --no-output > {filename}"
         run(prompt)
 
         with open(filename, "r") as file:
@@ -27,8 +25,13 @@ for length in lens:
 print()
 print()
 
+plt.xlabel("proc nums")
+plt.ylabel("exec time")
+plt.grid()
 for length in lens:
-    # if length == int(1e7):
-    plt.plot(*logs[length])
-    # break
+    x, y = logs[length]
+    plt.plot(x, y, label=str(length))
+plt.legend(loc="upper right")
+
+plt.savefig("plots/result_2.png")
 plt.show()
